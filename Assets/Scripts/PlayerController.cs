a1ba35632a891745f749844e36599ddf6b723372
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-31 15:38:34 -0700

    Add tag + Rigidbody to PickUp + set inactive on trigger
    Co-authored-by: Jon Cheng <joncheng.dev@gmail.com>
    Co-authored-by: Ian Bravo <bravo.ian@gmail.com>
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>
    Co-authored-by: Richard Barbour <richyb07@gmail.com>

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,41 +6,43 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Rigidbody of the player.
     private Rigidbody rb; 
-
-    // Movement along X and Y axes.
+    // private int count;
     private float movementX;
     private float movementY;
 
-    // Speed at which the player moves.
     public float speed = 0; 
 
-    // Start is called before the first frame update.
     void Start()
     {
-    // Get and store the Rigidbody component attached to the player.
+        // count = 0; 
         rb = GetComponent<Rigidbody>();
+        // winTextObject.SetActive(false);
+        // SetCountText();
     }
  
-    // This function is called when a move input is detected.
     void OnMove(InputValue movementValue)
     {
-    // Convert the input value into a Vector2 for movement.
         Vector2 movementVector = movementValue.Get<Vector2>();
-
-    // Store the X and Y components of the movement.
         movementX = movementVector.x; 
         movementY = movementVector.y; 
     }
 
-     // FixedUpdate is called once per fixed frame-rate frame.
     private void FixedUpdate() 
     {
-    // Create a 3D movement vector using the X and Y inputs.
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-
-    // Apply force to the Rigidbody to move the player.
         rb.AddForce(movement * speed); 
+    }
+
+     void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+            // count++; 
+            // SetCountText();
+            // pickupSound.clip = sfx;
+            // pickupSound.Play();
+        }
     }
 }
