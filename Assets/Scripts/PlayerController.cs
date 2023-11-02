a85adfe52374efbcae8dd9043c9a633201b6ce35
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-01 19:30:26 -0700

    Add AudioSource instance for jump sound fx

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    public AudioSource jumpSound;
     public AudioSource pickupSound;
     public Transform ballTransform;
     public TextMeshProUGUI countText;
@@ -51,6 +52,7 @@ public class PlayerController : MonoBehaviour
         {
             Vector3 ballJump = new Vector3(0.0f, 6.0f, 0.0f);
             rb.AddForce(ballJump * jumpSpeed);
+            jumpSound.Play();
         }
         isTouching = false;
         if (ballTransform.position.y < -1.0f)
